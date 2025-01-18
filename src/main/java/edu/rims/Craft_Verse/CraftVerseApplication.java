924b2968b0a5c61814d5019a0566e73721426383
Madhusmita-3 <sahoomadhusmita495@gmail.com>
2025-01-18 11:41:18 +0530

    initial env setup

---
+++ src/main/java/edu/rims/Craft_Verse/CraftVerseApplication.java
@@ -0,0 +1,13 @@
+package edu.rims.Craft_Verse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rims/Craft_Verse/CraftVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rims.Craft_Verse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
