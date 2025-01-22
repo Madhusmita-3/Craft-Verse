14a495789d0eacbee44efe737b341b5eb95c68bc
Rajalaxmi-14 <rajalaxmimajhi3009@gmail.com>
2025-01-22 11:07:23 +0530

    css issue fix

--- src/main/java/edu/rims/craft_verse/ProductController.java
+++ src/main/java/edu/rims/craft_verse/ProductController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @Controller
 @RequestMapping("/customer")
 public class ProductController {
+
     @GetMapping("/home")
     String customerHome() {
         return "customer/home";
     }
+    
+    
     
 }
