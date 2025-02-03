65fd9aa67e6d9364b13619826385aea42166dc83
Madhusmita-3 <sahoomadhusmita495@gmail.com>
2025-02-03 22:32:22 +0530

    banner changes

--- src/main/java/edu/rims/craft_verse/controllers/ProductController.java
+++ src/main/java/edu/rims/craft_verse/controllers/ProductController.java
@@ -1,5 +1,20 @@
 package edu.rims.craft_verse.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@RequestMapping("/customer")
 public class ProductController {
+
+    @GetMapping("/home")
+    String customerHome() {
+        return "customer/home";
+    }
+    
+    
     
 }
