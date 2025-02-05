1872c3fc0d9185390cc9c21e33eda0ad802a1007
Madhusmita-3 <sahoomadhusmita495@gmail.com>
2025-02-05 10:57:07 +0530

    db changes

--- database/script.sql
+++ database/script.sql
@@ -6,13 +6,62 @@ USE craft-verse;
 
 CREATE TABLE category (
     category_id INT AUTO_INCREMENT PRIMARY KEY,      
-    category_title VARCHAR(100) NOT NULL,                     
+    category_title TEXT NOT NULL,                     
     category_description TEXT, 
     category_image_url VARCHAR(255),                         
-    category_status ENUM('Active', 'Inactive') DEFAULT 'Active',
-    created_by INT,                                 
+    category_status ENUM('ACTIVE', 'INACTIVE') DEFAULT 'Active',
+    created_by INT,                                
     updated_by INT,                                 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
     
+);
+
+CREATE TABLE user (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    address TEXT,
+    profile_picture VARCHAR(255),
+    is_verified BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE order (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    buyer_id INT NOT NULL,
+    order_total_price DECIMAL(10,2) NOT NULL,
+    order_status ENUM('Pending', 'Shipped', 'Delivered', 'Cancelled') DEFAULT 'Pending',
+    payment_status ENUM('Paid', 'Unpaid', 'Refunded') DEFAULT 'Unpaid',
+    payment_method ENUM('Credit Card', 'PayPal', 'UPI', 'COD'),
+    tracking_id VARCHAR(50),
+    shipping_address TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DOUBLE(10,2) NOT NULL,
+);
+
+CREATE TABLE review (
+    review_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    craft_id INT NOT NULL,
+    rating DECIMAL(3,2) CHECK (Rating BETWEEN 1 AND 5),
+    review_text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE wishlist (
+    wishlist_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT, 
+    craft_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
