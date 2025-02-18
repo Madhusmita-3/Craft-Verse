2a023008443636544c72ed001d9aedf28832d3d5
Madhusmita-3 <sahoomadhusmita495@gmail.com>
2025-02-18 11:24:20 +0530

    plp changes

--- src/main/java/edu/rims/craft_verse/controllers/ProductController.java
+++ src/main/java/edu/rims/craft_verse/controllers/ProductController.java
@@ -1,5 +1,36 @@
 package edu.rims.craft_verse.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@RequestMapping("/customer")
 public class ProductController {
+
+    @GetMapping("/home")
+    String customerHome() {
+        return "customer/home";
+    }
+    
+    @GetMapping("/plp")
+    String customerPlp() {
+        return "customer/plp";
+    }
+    @GetMapping("/pdp")
+    String customerPdp() {
+        return "customer/pdp";
+    }
+    @GetMapping("/signup")
+    String customerSignup() {
+        return "customer/signup";
+    }
+    @GetMapping("/login")
+    String customerLogin() {
+        return "customer/login";
+    }
+    
     
 }
