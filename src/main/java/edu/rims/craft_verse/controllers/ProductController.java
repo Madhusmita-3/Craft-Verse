3f771625f36c82019fa909147d7d5625cbd2a33e
Rajalaxmi-14 <rajalaxmimajhi3009@gmail.com>
2025-02-20 12:47:40 +0530

    conflict solved

--- src/main/java/edu/rims/craft_verse/controllers/ProductController.java
+++ src/main/java/edu/rims/craft_verse/controllers/ProductController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 @RequestMapping("/customer")
 public class ProductController {
@@ -14,26 +12,25 @@ public class ProductController {
     String customerHome() {
         return "customer/home";
     }
-    
-<<<<<<< HEAD
-=======
+
     @GetMapping("/plp")
     String customerPlp() {
         return "customer/plp";
     }
+
     @GetMapping("/pdp")
     String customerPdp() {
         return "customer/pdp";
     }
+
     @GetMapping("/signup")
     String customerSignup() {
         return "customer/signup";
     }
+
     @GetMapping("/login")
     String customerLogin() {
         return "customer/login";
     }
->>>>>>> home-page
-    
-    
+
 }
